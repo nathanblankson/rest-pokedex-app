4a7d335e97cdb91e70a5835d92bffd62c78ec460
Nathan Blankson <nathan@sourcebreaker.com>
2021-05-20 08:38:10 +0100

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         HttpClientModule,
         AppRoutingModule
     ],
