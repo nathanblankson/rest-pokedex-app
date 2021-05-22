7787507948424864a8efa6aeabe8ec38f5591490
Nathan Blankson <nathan@sourcebreaker.com>
2021-05-22 12:47:29 +0100

    Updated list item

--- src/app/+pokemon/components/pokemon-list-item/pokemon-list-item.component.ts
+++ src/app/+pokemon/components/pokemon-list-item/pokemon-list-item.component.ts
@@ -24,6 +24,56 @@ export class PokemonListItemComponent implements OnInit {
                 }
             }
         ],
+        stats: [
+            {
+                base_stat: 45,
+                effort: 0,
+                stat: {
+                    name: "hp",
+                    url: "https://pokeapi.co/api/v2/stat/1/"
+                }
+            },
+            {
+                base_stat: 49,
+                effort: 0,
+                stat: {
+                    name: "attack",
+                    url: "https://pokeapi.co/api/v2/stat/2/"
+                }
+            },
+            {
+                base_stat: 49,
+                effort: 0,
+                stat: {
+                    name: "defense",
+                    url: "https://pokeapi.co/api/v2/stat/3/"
+                }
+            },
+            {
+                "base_stat": 65,
+                "effort": 1,
+                "stat": {
+                    "name": "special-attack",
+                    "url": "https://pokeapi.co/api/v2/stat/4/"
+                }
+            },
+            {
+                "base_stat": 65,
+                "effort": 0,
+                "stat": {
+                    "name": "special-defense",
+                    "url": "https://pokeapi.co/api/v2/stat/5/"
+                }
+            },
+            {
+                "base_stat": 45,
+                "effort": 0,
+                "stat": {
+                    "name": "speed",
+                    "url": "https://pokeapi.co/api/v2/stat/6/"
+                }
+            }
+        ],
         image_url: "https://pokeres.bastionbot.org/images/pokemon/1.png"
     };
 
