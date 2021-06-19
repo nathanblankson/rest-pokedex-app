bfa4bb433f27b34cf11e17c57d02b6dee7abd358
Nathan Blankson <nathan@sourcebreaker.com>
2021-06-19 19:15:07 +0100

    Basic responsive sidenav

--- src/app/core/layout/navigation/nav-wrapper.component.ts
+++ src/app/core/layout/navigation/nav-wrapper.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
     selector: 'app-nav-wrapper',
     templateUrl: './nav-wrapper.component.html',
     styleUrls: ['./nav-wrapper.component.scss']
 })
-export class NavWrapperComponent implements OnInit {
+export class NavWrapperComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    @ViewChild('sidenav')
+    public sidenav: MatSidenav;
+
+    public mobileQuery: MediaQueryList;
+
+    private _mobileQueryListener: () => void;
+
+    constructor(private changeDetectorRef: ChangeDetectorRef, private media: MediaMatcher) {
+        this.mobileQuery = media.matchMedia('(max-width: 600px)');
+        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+        this.mobileQuery.addListener(this._mobileQueryListener);
+    }
 
     public ngOnInit(): void {
+    }
+
+    public toggleSidenavEmitted(e) {
+        this.sidenav.toggle();
+    }
+
+    ngOnDestroy(): void {
+        this.mobileQuery.removeListener(this._mobileQueryListener);
     }
 
 }
