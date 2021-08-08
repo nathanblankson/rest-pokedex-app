2dfb502a7cc614e69607b9fe60d531f0ec6fe10a
Nathan Blankson <nathan@sourcebreaker.com>
2021-08-08 12:34:04 +0100

    Fix ESLint quotes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,7 +21,7 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'rest-pokedex-app'`, () => {
+    it('should have as title "rest-pokedex-app"', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app.title).toEqual('rest-pokedex-app');
