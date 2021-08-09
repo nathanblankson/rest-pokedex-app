787874053f6ecd3d534e00ec3acf488fc32c8158
Nathan Blankson <nathan@sourcebreaker.com>
2021-08-09 07:38:20 +0100

    Nav use router-outlet instead of ng-content

--- src/app/+pokemon/pokemon-routing.module.ts
+++ src/app/+pokemon/pokemon-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NavWrapperComponent } from '@core/layout/navigation/nav-wrapper.component';
 import { PokemonResolver } from '@core/resolvers/pokemon.resolver';
 import { PokemonComponent } from './containers/pokemon/pokemon.component';
 import { PokemonDetailsComponent } from './containers/pokemon-details/pokemon-details.component';
@@ -8,14 +9,11 @@ import { PokemonDetailsComponent } from './containers/pokemon-details/pokemon-de
 const routes: Routes = [
     {
         path: '',
-        component: PokemonComponent
-    },
-    {
-        path: ':name',
-        component: PokemonDetailsComponent,
-        resolve: {
-            pokemon: PokemonResolver
-        }
+        component: NavWrapperComponent,
+        children: [
+            { path: '', component: PokemonComponent },
+            { path: ':name', component: PokemonDetailsComponent, resolve: { pokemon: PokemonResolver } }
+        ]
     }
 ];
 
--- src/app/+wishlist/wishlist-routing.module.ts
+++ src/app/+wishlist/wishlist-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NavWrapperComponent } from '@core/layout/navigation/nav-wrapper.component';
 import { WishlistComponent } from './containers/wishlist/wishlist.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: WishlistComponent
+        component: NavWrapperComponent,
+        children: [
+            { path: '', component: WishlistComponent }
+        ]
     }
 ];
 
